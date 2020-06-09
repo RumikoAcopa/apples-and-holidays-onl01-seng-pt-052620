ed01eb9bb23cccd4df9be46301f916c75c9dafa9
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-09 01:23:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |key,value|
-    
+
 
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
